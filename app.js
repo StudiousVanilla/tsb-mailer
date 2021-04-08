ecbc1bc1f504a7cf4afec7c5362f729074ef8001
Oisín_windows <byrne.ois@gmail.com>
2021-04-08 10:40:53 +0100

    added backend data fetching

--- app.js
+++ app.js
@@ -7,6 +7,24 @@ const validator = require("email-validator");
 //cookies
 const cookieSession = require("cookie-session")
 
+
+//firebase connection
+const firebase = require('firebase/app');
+require('firebase/firestore');
+require('firebase/auth');
+require('firebase/storage');
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGEBUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID
+};
+const fireDone = firebase.initializeApp(firebaseConfig)
+const db = fireDone.firestore()
+
 // middleware
 app.use(express.json())
 // Add headers
@@ -43,6 +61,143 @@ app.use(
     })
 );
 
+
+
+// gets all the published blogs
+app.get('/blogs', (req, res)=>{
+    
+    // pulls blog information based on number of blogs and if they are published
+    db.collection("blogs").where('published', '==', true)
+    .get()
+    .then((querySnapshot) => {
+        // addes blog data to blogsArray
+        let blogArray = []
+        querySnapshot.docs.forEach(doc =>{
+            blogArray.push(doc.data())
+        })
+
+        // sort blogArray by date
+        const sortedArray = blogArray.sort((a, b) => a.date > b.date ? -1 : 1)
+
+        // update Sate
+        console.log(sortedArray)
+
+        res.send(sortedArray)
+    })
+    .catch((error) => {
+        console.log("Error getting documents: ", error);
+    });
+
+})
+
+// gets specific blog post
+// needs a blog reference in the request body
+app.get('/blogpost', (req, res)=>{
+
+    const blogNumber = req.body.ref
+
+    console.log(req.body.ref)
+    
+    // pulls blog information for blog that matches title (the blogRef)
+    db.collection("blogs").where('blogID', '==', blogNumber)
+    .get()
+    .then((querySnapshot) => {
+        // addes blog data to blogsArray
+        let blog = null
+        querySnapshot.docs.forEach(doc =>{
+            blog = doc.data()
+        })
+
+        // update Sate
+        res.send(blog)
+    })
+    .catch((error) => {
+        console.log("Error getting documents: ", error);
+    });
+
+})
+
+// gets blogs filtered by theme
+// needs a theme in the request body
+app.get('/blogfilter', (req, res)=>{
+
+    const theme = req.body.theme
+
+    console.log(req.body.theme)
+    
+    // pulls blog information based on if they are published and if they match the filtered theme from the url params
+    db.collection("blogs")
+    .where('published', '==', true)
+    .where('theme', '==', theme)
+    .get()
+    .then((querySnapshot) => {
+        // adds blog data to blogsArray
+        let blogArray = []
+        querySnapshot.docs.forEach(doc =>{
+            blogArray.push(doc.data())
+        })
+
+        // sort blogArray by date
+        const sortedArray = blogArray.sort((a, b) => a.date > b.date ? -1 : 1)
+
+        // update Sate
+        res.send(sortedArray)
+        
+    })
+    .catch((error) => {
+        console.log("Error getting documents: ", error);
+    });
+
+})
+
+// gets all blog themes, and most recent blog (for tsb Sidebar)
+// needs a theme in the request body
+app.get('/blogthemes', (req, res)=>{
+    
+    // pulls blog information based on number of blogs and if they are published
+    db.collection("blogs").where('published', '==', true)
+    .get()
+    .then((querySnapshot) => {
+        // addes blog data to blogsArray
+        let blogArray = []
+        querySnapshot.docs.forEach(doc =>{
+            let blog = doc.data()
+            blogArray.push(blog)
+        })
+
+        // sort blogArray by date
+        const sortedByDate = blogArray.sort((a, b) => a.date > b.date ? -1 : 1)
+        // grab mosr recent blog
+        let recentBlog = sortedByDate[0]
+
+        // holds all the themes from the blog database
+        let blogThemeArray = []
+        
+        // sort blogArray by date
+        const sortedByTheme = blogArray.sort((a, b) => a.theme > b.theme ? 1 : -1)
+
+        // filters blogs based on theme
+        const filteredArray = sortedByTheme.filter((blog)=>{
+            if(blogThemeArray.includes(blog.theme)){
+                
+            }
+            else{
+                blogThemeArray.push(blog.theme)
+                return blog
+            }
+            return null
+        })
+
+        res.send([recentBlog, filteredArray])
+
+    })
+    .catch((error) => {
+        console.log("Error getting documents: ", error);
+    });
+})
+
+
+// just to stop errors when testing Heroku app
 app.get('/', (req, res)=>{
     res.send('hello')
 })
